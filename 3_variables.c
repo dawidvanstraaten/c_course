a878c204b3d44c374310b4ea62057f81b60d0b73
Dawid van Straaten <dawidvanstraaten@gmail.com>
2025-01-04 20:57:26 +0200

    Add files via upload

---
+++ 3_variables.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    // We initialize two variables we'll use to calculate the sum from.
+    int num1 = 0, num2 = 0, sum = 0;
+    // Prompt the user to enter a number.
+    printf("Please enter a number: ");
+    scanf("%d", &num1);
+    // Prompt the user to enter another number.
+    printf("Please enter another number: ");
+    scanf("%d", &num2);
+    sum = num1 + num2;
+    printf("The sum of %d and %d is equal to %d", num1, num2, sum);
+}   
